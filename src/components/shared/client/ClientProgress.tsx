5ba79cece6e8464f9ff0483857544e90997fd0dd
Kevin <hello@kevinivan.com>
2024-03-05 20:50:25 +0800

    fake progress

--- src/components/shared/client/ClientProgress.tsx
+++ src/components/shared/client/ClientProgress.tsx
@@ -4,7 +4,7 @@ const ClientProgress = () => {
   return (
     <div className="space-y-6">
       <div>
-        <h3 className="text-lg font-medium">Vitalik's progress</h3>
+        <h3 className="text-lg font-medium">Kevin's fake progress</h3>
       </div>
       <TaskList />
     </div>
